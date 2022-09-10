4c89e8b6bfcd704b97fbce24accd1a1cdd0e18a0
bhanodai <bhanodaig@gmail.com>
2022-09-10 12:42:30 +0530

    initial commit

---
+++ scripts/fund.js
@@ -0,0 +1,21 @@
+const { getNamedAccounts } = require("hardhat");
+
+async function main() {
+  const { deployer } = await getNamedAccounts();
+  const fundMe = await ethers.getContract("FundMe", deployer);
+  console.log("Funding Contract..");
+  const transactionResponse = await fundMe.fund({
+    value: ethers.utils.parseEther("1"),
+  });
+  await transactionResponse.wait();
+  console.log("Funded");
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
